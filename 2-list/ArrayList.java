1d7e80f562811abc7b2acc01749f45ed112dcf18
rivlev <proflevitan@gmail.com>
2023-09-12 14:48:07 +0000

    class code 9/12

--- 2-list/ArrayList.java
+++ 2-list/ArrayList.java
@@ -1,3 +1,5 @@
+import java.lang.*;
+
 public class ArrayList<E> {
     Object[] data;
     int size;
@@ -18,8 +20,33 @@ public class ArrayList<E> {
 
     public boolean add(E elt) {
         if (size == data.length) grow();
-        newdata[size] = elt;
+        data[size] = elt;
         size++;
         return true;
+    }
+
+    public void add(int index, E elt) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        if (size == data.length) grow();
+        for (int i = size; i > index; i--)  {
+            data[i] = data[i-1];
+        }
+        data[index] = elt;
+        size++;
+    }
+
+    public void remove() {
+        data[size-1] = null;
+        size--;             // this is the important part!
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        E toRemove = (E)data[index];
+        for (int i = index; i < size-1; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+        return toRemove;
     }
 }
