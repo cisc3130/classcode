cfbf30d5cfb75dbc70dedb2fa298d3b26c199c58
rivlev <proflevitan@gmail.com>
2023-09-14 14:47:20 +0000

    Thursday 9/14 code

--- 2-list/ArrayList.java
+++ 2-list/ArrayList.java
@@ -1,11 +1,12 @@
 import java.lang.*;
+import java.util.Arrays;
 
 public class ArrayList<E> {
     Object[] data;
     int size;
 
     public ArrayList() {
-        ArrayList(10);
+        this(10);
     }
 
     public ArrayList(int initialCapacity) {
@@ -16,6 +17,13 @@ public class ArrayList<E> {
     protected void grow() {
         Object[] newdata = Arrays.copyOf(data, data.length*2);
         data = newdata;
+    }
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
     }
 
     public boolean add(E elt) {
@@ -48,5 +56,20 @@ public class ArrayList<E> {
         }
         size--;
         return toRemove;
+    }
+
+    public E get(int index) {
+        return (E) data[index];
+    }
+
+    public E set(int index, E newValue) {
+        E oldValue = (E) data[index];
+        data[index] = newValue;
+        return oldValue;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> strlist = new ArrayList<>();
+
     }
 }
