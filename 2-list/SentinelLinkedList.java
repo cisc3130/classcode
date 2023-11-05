101e909a6fc6342791185600021d569cb0b80c49
rivlev <proflevitan@gmail.com>
2023-11-05 02:15:35 +0000

    Thursday 11-2 class code

--- 2-list/SentinelLinkedList.java
+++ 2-list/SentinelLinkedList.java
@@ -44,6 +44,28 @@ public class SentinelLinkedList<E> {
         return true;
     }
 
+    public boolean removeLastOccurrence(E target) {
+        // iterate through the list
+        // when you find an instance of the element, keep a reference to it
+        // update the reference if you find another instance of the element so it's always pointing to the last one
+        // when you reach the end of the list, remove the last instance that you found.
+
+        // iterate through the list: use a tracker node, start at sentinel, end at sentinel, move forward using next
+        Node tnd = sentinel.next;       // sentinel.next points to the first node
+        Node dnd = null;
+        while (tnd != sentinel) {       // end when you reach sentinel. don't enter the loop if the list is empty (sentinel.next == sentinel)
+            if (tnd.data.equals(target)) dnd = tnd;
+        }
+        if (dnd == null) return false;
+
+        dnd.prev.next = dnd.next;
+        dnd.next.prev = dnd.prev;       // without the sentinel, you would have to make sure dnd.prev and/or dnd.next exist
+        return true;
+
+        // This is inefficient. We should really start at the end of the list (Thank you Abir)
+        // Exercise for you offline
+    }
+
     // midterm practice: implement the optimization of starting
     // from the end that the index is closest to. This applies
     // to get, remove, add
--- 5-logarithmic-sorts/Sorter.java
+++ 5-logarithmic-sorts/Sorter.java
@@ -2,6 +2,69 @@ import java.lang.Math;
 
 public class Sorter {
 
+    public static <E extends Comparable<E>> void quickSort(E[] arr) {
+        quickSort(arr, 0, arr.length);
+    }
+
+    // when this instance of quickSort returns, 
+    // arr [start, end) will be sorted
+    public static <E extends Comparable<E>> void quickSort(E[] arr, int start, int end) {
+        System.out.println(String.format("Calling quicksort on range [%d, %d)", start, end));
+        // base case
+        if (end - start <= 1) {
+            System.out.println(String.format("Base case: %d - %d = %d", end, start, end-start));
+            return;   // array of length 0 or 1 is trivially sorted
+        }
+
+        // partition
+        int pivot = partition(arr, start, end);
+        System.out.println(String.format("Partition step completed. Pivot is at index %d.", pivot));
+        printArr(arr, start, end);
+
+        // recurse
+        System.out.println("Recursing on elements smaller than pivot");
+        quickSort(arr, start, pivot);
+        System.out.println("Recursing on elements larger than pivot");
+        quickSort(arr, pivot+1, end);
+
+        // entire range [start, end) is now sorted
+        System.out.println(String.format("arr[start, end) is now sorted"));
+        printArr(arr, start, end);
+    }
+
+    // choose a pivot, partition all elements in the range on either side
+    // of the pivot, return the final index of the pivot
+    protected static <E extends Comparable<E>> int partition(E[] arr, int start, int end) {
+        int pivotIdx = choosePivot(arr, start, end);
+
+        // swap pivot to the end
+        E tmp = arr[pivotIdx];
+        arr[pivotIdx] = arr[end-1];
+        arr[end-1] = tmp;
+
+        int afterLastSmallIdx = 0, firstUnseenIdx = 0;
+        while (firstUnseenIdx < end-1) {        // loop until we reach the pivot
+            if (arr[firstUnseenIdx].compareTo(arr[end-1]) < 0) {
+                tmp = arr[firstUnseenIdx];
+                arr[firstUnseenIdx] = arr[afterLastSmallIdx];
+                arr[afterLastSmallIdx] = tmp;
+                afterLastSmallIdx++;
+            }
+            firstUnseenIdx++;
+        }
+
+        // swap pivot into correct location
+        tmp = arr[afterLastSmallIdx];
+        arr[afterLastSmallIdx] = arr[end-1];
+        arr[end-1] = tmp;
+
+        return afterLastSmallIdx;
+    }
+
+    protected static <E extends Comparable<E>> int choosePivot(E[] arr, int start, int end) {
+        return (int) Math.random() * (end-start) + start;
+    }
+
     public static <E extends Comparable<E>> void mergeSort(E[] arr) {
         mergeSort(arr, 0, arr.length);
     }
@@ -62,8 +125,8 @@ public class Sorter {
 
     public static void main(String[] args) {
         Integer[] arr = { 7, 1, 8, 9, 5, 0, 2, 3 };
-        mergeSort(arr);
-        printArr(arr);
+        quickSort(arr);
+        // printArr(arr);
     }
 
 } // end Sorter
