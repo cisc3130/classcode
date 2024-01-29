b5af635b138bc576af08ba400d37e07a795360a6
john0x44 <136937612+john0x44@users.noreply.github.com>
2024-01-29 18:51:27 +0000

    Added linear and quadratic algorithm

--- 1-algorithms-and-sorts/Analysis.java
+++ 1-algorithms-and-sorts/Analysis.java
@@ -2,7 +2,7 @@ public class Analysis {
 
     // print every element in an array
     public void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) 
+        for (int i = 0; i < arr.length; i++){
             for (int j = 0; j < 3; j++) {
                 System.out.println(arr[i] + " ");
             }
@@ -11,8 +11,33 @@ public class Analysis {
     }
 
 
+
     // sum up all elements in an array
     
+    // linear algorithm ; see hw1.md (a)
+    // Linear search 
+    public static int linearAlgorithm(int[] arr, int num){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == num){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // quadratic algorithm ; see hw1.md (b)
+    // Sum all elements then multiply by array size
+    public static int quadraticAlgorithm(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            int arrSum = 0;
+            for(int j = 0; j < arr.length; j++){
+                arrSum += arr[j];
+            }
+            sum += arrSum;
+        }
+        return sum;
+    }
 
     // see if an array contains any duplicates
     public boolean containsDuplicates(int[] arr) {
@@ -26,7 +51,12 @@ public class Analysis {
 
 
 
+
     public static void main(String[] args) {
 
+        // // Testing 
+        // int [] arr = {1,2,3,4,5,6};
+        // System.out.println(linearAlgorithm(arr, 10));
+        // System.out.println(quadraticAlgorithm(arr));
     }
 }
