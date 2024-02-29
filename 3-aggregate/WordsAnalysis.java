3556bbe7ac0bb46e9e874c30bdeb85ce5ac87073
rivlev <rlevitan@brooklyn.cuny.edu>
2024-02-29 15:46:56 +0000

    Thursday 2-29 class code

---
+++ 3-aggregate/WordsAnalysis.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class WordsAnalysis {
+    List<String> words;
+
+
+    public WordsAnalysis() {
+        words = new ArrayList<>();
+        File f = new File("/workspaces/classcode/words_alpha.txt");
+        try {
+            Scanner freader = new Scanner(f);
+            while (freader.hasNextLine()) {
+                words.add(freader.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        WordsAnalysis w = new WordsAnalysis();
+
+        // average length of words that start with x
+        w.words.stream()
+            .filter(s -> s.length() > 12 && s.charAt(0) == s.charAt(s.length()-1))
+            .filter(s -> s.charAt(0) != 't' && s.charAt(0) != 's')
+            .forEach(System.out::println);
+        
+    }
+
+}
