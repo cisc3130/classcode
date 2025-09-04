5d1430264dd201d3c4a932259ebca8cec897ab3f
Rebecca Levitan <rlevitan@brooklyn.cuny.edu>
2025-09-04 14:37:08 +0000

    Thurs 9-4 class code

--- 1-algorithms-and-sorts/GenericSorter.java
+++ 1-algorithms-and-sorts/GenericSorter.java
@@ -8,7 +8,7 @@ class Desk implements Comparable<Desk> {
     }
 }
 
-public class GenericSorter<E> {
+public class GenericSorter<E extends Comparable<E>> {
 
     public String[] generateStringArray() {
         String[] arr = new String[10];
@@ -29,15 +29,13 @@ public class GenericSorter<E> {
     public void insertionSort(E[] arr) {
         for (int i = 1; i < arr.length; i++) {
             E toSort = arr[i];
-            int j = i;
-            while (j > 0 && toSort.compareTo(arr[j-1]) < 0) {
-                arr[j] = arr[j-1];
-                j--;
+            int j = i-1;
+            for (; (j >= 0) && (arr[j].compareTo(toSort) > 0); j--) {
+                arr[j+1] = arr[j];
             }
-            arr[j] = toSort;
+            arr[j+1] = toSort;
         }
     }
-    
 
 
     public static void main(String[] args) {
@@ -48,13 +46,13 @@ public class GenericSorter<E> {
         sorter.insertionSort(arr);
         printArr(arr);
 
-        Integer[] intarr = { 3, 4, 2, 6, 8, 5, 3 };
-        GenericSorter<Integer> intSorter = new GenericSorter<>();
-        printArr(intarr);
+        // Integer[] intarr = { 3, 4, 2, 6, 8, 5, 3 };
+        // GenericSorter<Integer> intSorter = new GenericSorter<>();
+        // printArr(intarr);
 
-        Desk[] deskarr = { new Desk(), new Desk() };
-        printArr(deskarr);
-        GenericSorter<Desk> deskSorter = new GenericSorter<>();
+        // Desk[] deskarr = { new Desk(), new Desk() };
+        // printArr(deskarr);
+        // GenericSorter<Desk> deskSorter = new GenericSorter<>();
 
         // GenericSorter<char> basicSorter = new GenericSorter<>();
 
