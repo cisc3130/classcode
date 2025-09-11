48088828f5a1c1307402ced7819daeb61d0a99bc
Rebecca Levitan <rlevitan@brooklyn.cuny.edu>
2025-09-11 14:46:50 +0000

    Thursday 9-11 class code

--- 2-list/MArrayList.java
+++ 2-list/MArrayList.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class MArrayList<E> {
+public class MArrayList<E> extends AbstractList<E> {
     E [] data;
     int size;
 
@@ -24,14 +24,52 @@ public class MArrayList<E> {
         return true;
     }
 
+    public void add(int idx, E elt) {
+        if (idx < 0 || idx > size) throw new IndexOutOfBoundsException();
+        if (size == data.length) grow();
+        for (int i = size; i > idx; i--) {
+            data[i] = data[i-1];
+        }
+        data[idx] = elt;
+        size++;
+    }
+
+    public E remove(int idx) {
+        if (idx < 0 || idx >= size) throw new IndexOutOfBoundsException();
+        E toReturn = data[idx];
+        for (int i = idx; i < size - 1; i++) {
+            data[i] = data[i+1];
+        }
+        size--;
+        return toReturn;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public E get(int idx) {
+        if (idx < 0 || idx >= size) throw new IndexOutOfBoundsException();
+        return data[idx];
+    }
+
+    public E set(int idx, E elt) {
+        if (idx < 0 || idx >= size) throw new IndexOutOfBoundsException();
+        E toReturn = data[idx];
+        data[idx] = elt;
+        return toReturn;
+    }
+
     public static void main(String[] args) {
         MArrayList<String> lst = new MArrayList<>(3);
         lst.add("hello");
         lst.add("goodbye");
         lst.add("coffee");
-        lst.add("desk");
-        lst.add("phone");
+        lst.add(0, "desk");
+        lst.add(1, "phone");
         lst.add("computer");
         lst.add("chair");
+        lst.remove(lst.size()-1);
+        lst.remove(1);
     }
 }
--- 2-list/MLinkedList.java
+++ 2-list/MLinkedList.java
@@ -1,111 +1,34 @@
 public class MLinkedList<E> {
-    protected class Node {
-        E data;
+
+    class Node {
+        E elt;
         Node next;
-        Node(E data) { this.data = data; }
+        Node(E elt) {
+            this.elt = elt;
+        }
     }
 
     Node head;
     int size;
 
-    public MLinkedList() {}
-
-    public boolean isEmpty() { return head == null; }
-
-    public boolean add(E elt) {             // add elt to the end of the list
-        // create the new node
-        Node nnd = new Node(elt);
-
-        // edge case: if the list is empty
-        if (head == null) {
-            head = nnd;
-        } else {    // most cases: if list is not empty:
-            // get to the end of the list
-            Node tnd = head;
-            while (tnd.next != null) tnd = tnd.next;
-            // tnd now is the address of the last node (tnd.next == null)
-            
-            // link the last node to the new node
-            tnd.next = nnd;
-        }
-
-        // increment size
-        size++;
-
-        return true;
+    public MLinkedList() {
+        head = null;
+        size = 0;
     }
 
-    public E remove(int idx) {
-        if (idx < 0 || idx >= size) throw new IndexOutOfBoundsException();
-
-        // edge case: what if the node to be removed is the first one
-        if (idx == 0) {
-            head = head.next;
-        }
-
-        // get to the right place
+    public boolean add(E elt) {
+        // get to the end of the list
         Node tnd = head;
-        for (int i = 0; i < idx-1; i++) tnd = tnd.next;
-        // tnd is now pointing to the node before the one we will remove
-
-        // save the value to be returned
-        E toReturn = tnd.next.data;
-
-        // tnd.next is pointing to the node to be removed. Link it instead
-        // to the node after that
-        tnd.next = tnd.next.next;
-
-        // decrement size
-        size--;
-
-        return toReturn;
-    }
-
-    public E remove(E valueToRemove) {
-        // traverse through the list
-        // compare each node to the target value
-        // if the value is equal, remove the node
-
-        // edge case: what if the list is empty
-        if (head == null) {
-            return null;
-        }
-
-        // edge case: what if the value to remove is in the first node
-        if (head.data.equals(valueToRemove)) {
-            head = head.next;
-            size--;
-            return valueToRemove;
-        }
-
-        Node tnd = head;
-        while (tnd.next != null && !tnd.next.data.equals(valueToRemove)) {
+        while (tnd.next != null) {
             tnd = tnd.next;
         }
-        // tnd.next.data is now equal to valueToRemove OR tnd.next is null and the value is not in the list
-        if (tnd.next == null) {     // valueToRemove is not in list
-            return null;
-        }
-        
-        // tnd.next is not null which means that tnd.next.data is equal to valueToRemove
-        tnd.next = tnd.next.next;
-        
-        size--;
-        return valueToRemove;
+        // tnd.next is null := tnd is the last nd := this is the node we need to insert after
+        // allocate memory
+        Node nnd = new Node(elt);
+        // link it up
+        tnd.next = nnd;
+        // increment size
+        size++;
     }
-
-    public static void main(String[] args) {
-        String[] strs = { "hello", "goodbye", "cat", "dog", "coffee", "computer"};
-        MLinkedList<String> lst = new MLinkedList<>();
-        lst.add("hello");
-        int x = 72;
-        lst.add("goodbye");
-        String s = "data structures";
-        String t = "operating systems";
-        lst.add("cat");
-        int y = 89;
-        int z = 4;
-        lst.add("coffee");
-        lst.remove(2);
-    }
+   
 }
