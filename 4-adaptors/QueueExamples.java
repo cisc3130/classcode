5683e42a003e423d50b5152e2f18a69c846d97a5
Rebecca Levitan <rlevitan@brooklyn.cuny.edu>
2025-10-30 14:47:57 +0000

    Thurs 10-30 class code

--- 4-adaptors/QueueExamples.java
+++ 4-adaptors/QueueExamples.java
@@ -4,15 +4,15 @@ public class QueueExamples {
 
     public static boolean checkPalindrome(String str) {
         Stack<Character> s = new Stack<>();
-        Queue<Character> q = new java.util.LinkedList<>();
+        Queue<Character> q = new ArrayDeque<>();
 
         for (char c : str.toCharArray()) {
             s.push(c);
-            q.add(c);
+            q.offer(c);
         }
 
         while (!s.isEmpty()) {
-            if (!s.pop().equals(q.remove())) {
+            if (!s.pop().equals(q.poll())) {
                 return false;
             }
         }
@@ -54,6 +54,63 @@ public class QueueExamples {
         return true;
     }
 
+    // queue move target to front
+    public <E> void moveTargetToFront(Queue<E> q, E target) {
+        Queue<E> aux = new LinkedList<>();
+        while (!q.isEmpty()) {
+            E x = q.poll();
+            if (!x.equals(target)) {
+                aux.offer(x);
+            }
+            while (!q.isEmpty()) {
+                aux.add(q.remove());
+            }
+            q.add(target);
+
+            while (!aux.isEmpty()) {
+                q.add(aux.remove());
+            }
+        }
+    }
+
+    // queue move nth to front
+    public <E> void moveNthToFront(Queue<E> q, int n) {
+        Queue<E> aux = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            aux.add(q.remove());
+        }
+        E nth = q.remove();
+        while (!q.isEmpty()) {
+            aux.add(q.remove());
+        }
+        q.add(nth);     // this goes in once the queue has been emptied so it can be first in the queue
+        // now add all the other elements in after it
+        while (!aux.isEmpty()) {
+            q.add(aux.remove());
+        }
+    }
+
+    // reverse the first half of the queue
+    public static <E> void reverseFirstHalfOfQueue(Queue<E> q) {
+        int half = q.size()/2;
+        Stack<E> s = new Stack<>();
+        for (int i = 0; i < half; i++) {
+            s.push(q.poll());
+        }
+        Queue<E> aq = new ArrayDeque<>();       // empty the second half into an auxiliary queue
+        while (!q.isEmpty()) {                  // so we can put the first half back into an empty queue
+            aq.offer(q.poll());                 // so it can still be first
+        }
+        while (!s.isEmpty()) {      // now that the queue is empty put first half back in reverse order
+            q.offer(s.pop());
+        }
+        while (!aq.isEmpty()) {     // and then put back second half
+            q.offer(aq.poll());
+        }
+    }
+
+
+
     public static void main(String[] args) {
         Queue<Integer> q1 = new java.util.LinkedList<>(),
             q2 = new ArrayDeque<>();
@@ -63,12 +120,22 @@ public class QueueExamples {
             q2.offer(i);
         }
 
-        while (!q1.isEmpty()) {
-            System.out.println(q1.poll());
-            System.out.println(q2.remove());
+        // while (!q1.isEmpty()) {
+        //     System.out.println(q1.poll());
+        //     System.out.println(q2.remove());
+        // }
+        System.out.println();
+
+        for (int i : q2) {
+            System.out.print(i);
         }
+        System.out.println();
 
+        reverseFirstHalfOfQueue(q2);
 
+        for (int i : q2) {
+            System.out.print(i);
+        }
 
     }
 }
--- 4-adaptors/RadixSort.java
+++ 4-adaptors/RadixSort.java
@@ -3,22 +3,29 @@ import java.util.*;
 public class RadixSort {
 
     List<Integer> toSort;
-    MArrayList<Queue<Integer>> bins;
+    ArrayList<Queue<Integer>> bins;
+    int BASE = 10;
     int numDigits;
 
     public RadixSort(List<Integer> toSort, int numDigits) {
         this.toSort = toSort;
-        this.numDigits = numDigits;
-        bins = new MArrayList<>();                   // instantiate the bins data structure
-        for (int i = 0; i < 10; i++) {
-            bins.add(new CLinkedList<Integer>());    // instantiate an empty queue (implemented as a LL) for each bin
+        bins = new ArrayList<>(BASE);
+        for (int i = 0; i < BASE; i++) {
+            bins.add(new LinkedList<>());
         }
+        this.numDigits = numDigits;
     }
 
     public void distribute(int d) {
         for (Integer i : toSort) {
             // get the digit in place d
             String numStr = String.valueOf(i);
+            int dlen = numStr.length();
+            if (dlen <= d) {
+                // if there is no such digit, treat it as 0
+                bins.get(0).add(i);
+                continue;
+            }
             int dval = Character.getNumericValue(numStr.charAt(numStr.length() - 1 - d));
             // add i to the bin corresponding to dval
             bins.get(dval).add(i);          // bins is an ArrayList<Queue<Integer>>     
@@ -28,11 +35,10 @@ public class RadixSort {
     }
 
     public void collect() {
-        ListIterator<Integer> it = toSort.listIterator();
+        toSort.clear();
         for (Queue<Integer> b : bins) {
             while (!b.isEmpty()) {
-                it.next();
-                it.set(b.poll());
+                toSort.add(b.poll());
             }
         }
     }
@@ -40,7 +46,33 @@ public class RadixSort {
     public void sort() {
         for (int i = 0; i < numDigits; i++) {
             distribute(i);
+            printBins();
             collect();
+            printToSort();
         }
+    }
+
+    public void printBins() {
+        for (int i = 0; i < BASE; i++) {
+            System.out.print(i + ": ");
+            Queue<Integer> b = bins.get(i);
+            for (Integer val : b) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printToSort() {
+        for (Integer val : toSort) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(170, 45, 75, 90, 802, 24, 2, 66);
+        RadixSort rs = new RadixSort(new ArrayList<>(nums), 3);
+        rs.sort();
     }
 }
