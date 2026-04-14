34022370d82708b2b5ac91f1b7432a534a389863
Rebecca Levitan <rlevitan@brooklyn.cuny.edu>
2026-04-14 14:41:18 +0000

    Tuesday 4-14 class code

--- 6-trees/BinaryTree.java
+++ 6-trees/BinaryTree.java
@@ -35,42 +35,44 @@ public class BinaryTree<E> {
         //        /    \
         //       7      25
         //      / \    /  \
-        //     8   10  20   30
+        //     6   10  20   30
         return t;
     }
 
+    // LRV -> postorder traversal       6 10 7 20 30 25 18
+    // VLR -> preorder traversal        18 7 6 10 25 20 30
+    // LVR -> inorder traversal         6 7 10 18 20 25 30 
+
+
+
     public void printTree(BTNode nd) {
         if (nd == null) return;
-        printTree(nd.left);
-        printTree(nd.right);
-        System.out.print(nd.data + " ");
+        if (nd.left != null) printTree(nd.left);                     // L
+        if (nd.right != null) printTree(nd.right);                    // R
+        System.out.print(nd.data + " ");        // V
     }
+
 
     public int getSize(BTNode nd) {
         if (nd == null) return 0;
-        int leftSize = getSize(nd.left);
-        int rightSize = getSize(nd.right);
-        return leftSize + rightSize + 1;
+        if (nd.left == null && nd.right == null) return 1;
+        return getSize(nd.left) + getSize(nd.right) + 1;
     }
 
     public boolean contains(BTNode nd, E elt) {
         if (nd == null) return false;
-        return nd.data.equals(elt)         // is in the current node
-            || contains(nd.left, elt)       // is in the left subtree
-            || contains(nd.right, elt);      // is in the right subtree
+        return nd.data.equals(elt) || contains(nd.left, elt) || contains(nd.right, elt);
     }
 
-    public int depth(BTNode nd) {
+    public int height(BTNode nd) {
         if (nd == null) return -1;
-        return Math.max(depth(nd.left), depth(nd.right)) + 1;
+        return Math.max(height(nd.left), height(nd.right)) + 1;
     }
 
     public int countMatches(BTNode nd, E elt) {
         if (nd == null) return 0;
-        int leftMatches = countMatches(nd.left, elt);
-        int rightMatches = countMatches(nd.right, elt);
-        int currentMatch = nd.data.equals(elt) ? 1 : 0;
-        return leftMatches + rightMatches + currentMatch;
+        int this_matches = nd.data.equals(elt) ? 1 : 0;
+        return countMatches(nd.left, elt) + countMatches(nd.right, elt) + this_matches;
     }
 
     public int sumTree(BTNode nd) {
@@ -89,13 +91,12 @@ public class BinaryTree<E> {
 
     public BTNode copyTree(BTNode nd) {
         if (nd == null) return null;
-        // create a copy of the current node
-        BTNode cnd = new BTNode(nd.data);
-        BTNode cl = copyTree(nd.left), 
-            cr = copyTree(nd.right);
-        if (cl != null) cnd.addLeft(cl);
-        if (cr != null) cnd.addRight(cr);
-        return cnd;
+        BTNode nnd = new BTNode(nd.data);
+        nnd.left = copyTree(nd.left);
+        nnd.right = copyTree(nd.right);
+        if (nnd.left != null) nnd.left.parent = nnd;
+        if (nnd.right != null) nnd.right.parent = nnd;
+        return nnd;
     }
 
     
@@ -104,12 +105,13 @@ public class BinaryTree<E> {
 
     public static void main(String[] args) {
         BinaryTree<Integer> t = buildTree();
-        System.out.print("Postorder traversal of the tree: ");
+        System.out.print("Inorder traversal of the tree: ");
         t.printTree(t.root);
         System.out.println(); // For a new line after printing the tree
         // Output should be: 8 7 10 18 20 25 30
         // int depth = t.getTreeDepth(t.root);
         // System.out.println("Depth of the tree: " + depth);
+
     }
 
 
